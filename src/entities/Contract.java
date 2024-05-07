8b3eede1b6fbe3557bd6a20606e7e7555e9f293d
Eliezer Soares <eliezerdrum@gmail.com>
2024-05-07 10:28:44 -0300

    Domain Layer Design

---
+++ src/entities/Contract.java
@@ -0,0 +1,39 @@
+package entities;
+
+import java.util.Date;
+
+public class Contract {
+
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+
+	private Installment installment;
+
+	private Contract(Integer number, Date date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public Installment getInstallment() {
+		return installment;
+	}
+
+	public void setInstallment(Installment installment) {
+		this.installment = installment;
+	}
+
+}
---
+++ src/entities/Installment.java
@@ -0,0 +1,23 @@
+package entities;
+
+import java.util.Date;
+
+public class Installment {
+
+	private Date dueDate;
+	private Double amount;
+
+	public Installment(Date dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+}
